7a28cbb750130768dc80b43eaf96df8cb81b03a3
lata bansavad <latabansavade24@gmail.com>
2024-07-14 16:32:43 +0530

    url updated

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://chat-app-neo-dove.onrender.com",
     methods: ["GET", "POST"],
   },
 });
